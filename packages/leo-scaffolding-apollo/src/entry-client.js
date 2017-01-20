096ce452c19c5c65f561da1b144ef049d9e79155
Chris Biscardi <chris@christopherbiscardi.com>
2017-01-20 07:31:44 -0800

    prettier apollo

--- packages/leo-scaffolding-apollo/src/entry-client.js
+++ packages/leo-scaffolding-apollo/src/entry-client.js
@@ -1,40 +1,39 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import md5 from 'md5';
-import { print } from 'graphql';
+import React from "react";
+import { render } from "react-dom";
+import { Router, browserHistory } from "react-router";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import md5 from "md5";
+import { print } from "graphql";
 // Polyfill fetch
-import 'isomorphic-fetch';
+import "isomorphic-fetch";
 
-import routes from '@sa-labs/leo-core/build/load-routes';
+import routes from "@sa-labs/leo-core/build/load-routes";
 
 const gqlInterface = {
-  query({
-    query,
-    variables,
-    operationName,
-  }) {
-
-    const variablesHash = md5(Object.entries(variables)
-                                    .reduce((acc, [key, val]) => `${acc}:${key}:${val}`, ''));
+  query({ query, variables, operationName }) {
+    const variablesHash = md5(
+      Object
+        .entries(variables)
+        .reduce((acc, [ key, val ]) => `${acc}:${key}:${val}`, "")
+    );
     const queryHash = md5(print(query));
     return fetch(`/api/${queryHash}--${variablesHash}.json`).then(v => {
       return v.json();
     });
   }
-}
+};
 
 const client = new ApolloClient({
   networkInterface: gqlInterface,
-  initialState: window.__APOLLO_STATE__,
+  initialState: window.__APOLLO_STATE__
 });
 
-render((
+render(
   <ApolloProvider client={client}>
     <Router history={browserHistory}>
       {routes}
     </Router>
-  </ApolloProvider>
-), document.getElementById('content'));
+  </ApolloProvider>,
+  document.getElementById("content")
+);
--- packages/leo-scaffolding-apollo/src/entry-static.js
+++ packages/leo-scaffolding-apollo/src/entry-static.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import ReactDOM, { renderToStaticMarkup } from 'react-dom/server';
-import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import { getDataFromTree } from 'react-apollo/server';
-import { match, RouterContext, createMemoryHistory } from 'react-router';
-import path from 'path';
-import 'isomorphic-fetch';
-import { execute } from 'graphql';
-import { print } from 'graphql';
-const debug = require('debug')('leo-scaffolding-apollo:entry-static');
-import md5 from 'md5';
+import React from "react";
+import ReactDOM, { renderToStaticMarkup } from "react-dom/server";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { getDataFromTree } from "react-apollo/server";
+import { match, RouterContext, createMemoryHistory } from "react-router";
+import path from "path";
+import "isomorphic-fetch";
+import { execute } from "graphql";
+import { print } from "graphql";
+const debug = require("debug")("leo-scaffolding-apollo:entry-static");
+import md5 from "md5";
 
-import routes from '@sa-labs/leo-core/build/load-routes';
-import Html from '@sa-labs/leo-core/build/load-html';
-import {
-  conf,
-  schema
-} from '@sa-labs/leo-core/build/inserted-files';
+import routes from "@sa-labs/leo-core/build/load-routes";
+import Html from "@sa-labs/leo-core/build/load-html";
+import { conf, schema } from "@sa-labs/leo-core/build/inserted-files";
 
 const basePort = process.env.PORT || 3000;
 const apiHost = `http://localhost:${basePort + 10}`;
 const apiUrl = `${apiHost}/graphql`;
 
 const gqlInterface = {
-  query({
-    query,
-    variables,
-    operationName,
-  }) {
+  query({ query, variables, operationName }) {
     // TODO: static render is failing for some reason?!?!?
-    const variablesString = Object.entries(variables)
-                                  .reduce((acc, [key, val]) => `${acc}:${key}:${val}`, '');
+    const variablesString = Object
+      .entries(variables)
+      .reduce((acc, [ key, val ]) => `${acc}:${key}:${val}`, "");
     const queryHash = md5(print(query));
-    return execute(schema, query, undefined, undefined, variables, operationName).then(json => {
+    return execute(
+      schema,
+      query,
+      undefined,
+      undefined,
+      variables,
+      operationName
+    ).then(json => {
       _globalJSONAsset({
         name: `/api/${queryHash}--${md5(variablesString)}.json`,
         json: json
       });
       debug(json.length);
       return json;
-    })
+    });
   }
-}
-
+};
 
 export default (locals, callback) => {
   debug(`${locals.path} rendering`);
@@ -61,21 +61,23 @@ export default (locals, callback) => {
       </ApolloProvider>
     );
 
-    getDataFromTree(component).then(context => {
-      const body = ReactDOM.renderToString(component);
-      const html = renderToStaticMarkup(
-        <Html
-          body={body}
-          assets={locals.assets}
-          bundleAssets={locals.assetsPluginHash}
-          props={renderProps}
-          data={context.store.getState().apollo.data}
-        />
-      );
-      callback(null, html);
-    }).catch(e => {
-      debug(`${locals.path} failed`);
-      callback(e);
-    })
-  })
-}
+    getDataFromTree(component)
+      .then(context => {
+        const body = ReactDOM.renderToString(component);
+        const html = renderToStaticMarkup(
+          <Html
+            body={body}
+            assets={locals.assets}
+            bundleAssets={locals.assetsPluginHash}
+            props={renderProps}
+            data={context.store.getState().apollo.data}
+          />
+        );
+        callback(null, html);
+      })
+      .catch(e => {
+        debug(`${locals.path} failed`);
+        callback(e);
+      });
+  });
+};
