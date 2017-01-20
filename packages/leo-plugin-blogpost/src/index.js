4a47ac1c61c2a8a44ad0c08089c93e3302db0d65
Chris Biscardi <chris@christopherbiscardi.com>
2017-01-20 07:30:58 -0800

    prettier blogpost

--- packages/leo-plugin-blogpost/src/index.js
+++ packages/leo-plugin-blogpost/src/index.js
@@ -1,16 +1,13 @@
 module.exports = function configure(config) {
-
   config.module.rules.push({
     test: /\.post$/,
     exclude: /node_modules/,
-    use: [{
-      loader: '@sa-labs/leo-plugin-blogpost/loader'
-    },{
-      loader: '@sa-labs/leo-plugin-markdown/loader'
-    }, {
-      loader: 'frontmatter-loader'
-    }]
+    use: [
+      { loader: "@sa-labs/leo-plugin-blogpost/loader" },
+      { loader: "@sa-labs/leo-plugin-markdown/loader" },
+      { loader: "frontmatter-loader" }
+    ]
   });
 
   return config;
-}
+};
--- packages/leo-plugin-blogpost/src/process.js
+++ packages/leo-plugin-blogpost/src/process.js
@@ -1,26 +1,29 @@
-const oDebug = require('debug');
-const debug = oDebug('leo:plugin-blogpost:process');
+const oDebug = require("debug");
+const debug = oDebug("leo:plugin-blogpost:process");
 /**
  * Filter drafts out of the dataset by default
  * But keep them if we're told to
  */
 function maybeFilterDrafts(obj) {
-  const isBlogPost = obj.attributes.contentType === 'leo-blogpost';
+  const isBlogPost = obj.attributes.contentType === "leo-blogpost";
   const shouldRenderDrafts = Boolean(process.env.BLOGPOST_RENDER_DRAFTS);
 
-  if(isBlogPost && !shouldRenderDrafts) {
+  if (isBlogPost && !shouldRenderDrafts) {
     // Filter out any drafts if we shouldn't renderDrafts
-    if(obj.attributes.status === 'draft') {
-      debug('filtering out draft: ', obj.attributes.title || obj.attributes.slug);
+    if (obj.attributes.status === "draft") {
+      debug(
+        "filtering out draft: ",
+        obj.attributes.title || obj.attributes.slug
+      );
     }
 
-    return obj.attributes.status !== 'draft';
+    return obj.attributes.status !== "draft";
   } else {
     // passthrough for any non-blogposts since we shouldn't be handling them
-    return true
+    return true;
   }
 }
 
 module.exports = function(data, cb) {
   cb(null, data.filter(maybeFilterDrafts));
-}
+};
