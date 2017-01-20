f4ae75e04308dbe3c8aaf0651145ae57e0dc0a85
Chris Biscardi <chris@christopherbiscardi.com>
2017-01-20 07:31:44 -0800

    prettier apollo

--- packages/leo-scaffolding-apollo/src/create-apollo-client.js
+++ packages/leo-scaffolding-apollo/src/create-apollo-client.js
@@ -1,12 +1,20 @@
-import ApolloClient, { addTypename } from 'apollo-client';
+import ApolloClient, { addTypename } from "apollo-client";
 
-export default options => new ApolloClient(Object.assign({}, {
-  queryTransformer: addTypename,
-  dataIdFromObject: (result) => {
-    if (result.id && result.__typename) { // eslint-disable-line no-underscore-dangle
-      return result.__typename + result.id; // eslint-disable-line no-underscore-dangle
-    }
-    return null;
-  },
-  // shouldBatch: true,
-}, options));
+export default options => new ApolloClient(
+  Object.assign(
+    {},
+    {
+      queryTransformer: addTypename,
+      // shouldBatch: true,
+      dataIdFromObject: result => {
+        if (result.id && result.__typename) {
+          // eslint-disable-line no-underscore-dangle
+          return result.__typename +
+            result.id; // eslint-disable-line no-underscore-dangle
+        }
+        return null;
+      }
+    },
+    options
+  )
+);
